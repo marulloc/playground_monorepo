c3fa7f4d1cfa04b5eb0a8de622463376e34e14fb
marulloc <marullo@naver.com>
2023-11-16 18:08:32 +0900

    [refactor] nav item active class validation logic

--- apps-marulloc/marulloc-blog/src/components/Navigation/NavItem.tsx
+++ apps-marulloc/marulloc-blog/src/components/Navigation/NavItem.tsx
@@ -4,43 +4,17 @@ import { classNames } from "@/utils/classNames";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-// const NavItem = ({ href, children }: { href: string; children: React.ReactNode }) => {
-//   const pathname = usePathname();
-//   const isActive =
-//     decodeURIComponent(pathname.replace("/article", "")).startsWith(href.replace("/category", "")) ||
-//     decodeURIComponent(pathname.replace("/category", "")).startsWith(href.replace("/article", "")) ||
-//     decodeURIComponent(pathname).startsWith(href);
-
-//   return (
-//     <li>
-//       <Link
-//         href={href}
-//         className={classNames(
-//           "relative block px-3 py-2 transition",
-//           isActive ? "text-teal-500 dark:text-teal-400" : "hover:text-teal-500 dark:hover:text-teal-400",
-//         )}
-//       >
-//         {children}
-//         {isActive && (
-//           <span className="absolute inset-x-1 -bottom-px h-px bg-gradient-to-r from-teal-500/0 via-teal-500/40 to-teal-500/0 dark:from-teal-400/0 dark:via-teal-400/40 dark:to-teal-400/0" />
-//         )}
-//       </Link>
-//     </li>
-//   );
-// };
-
-const NavItem = ({
-  href,
-  type,
-  children,
-  highlight = false,
-}: {
+type TProps = {
   href: string;
   type: "dir" | "file";
   children: React.ReactNode;
-  highlight?: boolean;
-}) => {
-  const isActive = false;
+};
+
+const NavItem = ({ href, type, children }: TProps) => {
+  const pathname = usePathname();
+  const isActive = decodeURIComponent(pathname)
+    .replace(/^\/(article|category)\//, "")
+    .startsWith(href);
   return (
     <li>
       <Link
