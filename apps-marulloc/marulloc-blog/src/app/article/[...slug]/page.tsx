bb9f25c91d7a77b12436a491d41057d467c374f7
marulloc <marullo@naver.com>
2023-11-20 07:49:11 +0900

    [feature] basic article sibling list component

--- apps-marulloc/marulloc-blog/src/app/article/[...slug]/page.tsx
+++ apps-marulloc/marulloc-blog/src/app/article/[...slug]/page.tsx
@@ -1,35 +1,15 @@
 import MarkdownContents from "@/components/MarkdownContents";
 import { getParsedMarkdown } from "@/services/getParsedMarkdown";
-import { getSiblingList } from "@/services/getSiblingList";
-import Link from "next/link";
 
 const Page = async ({ params }: any) => {
   const modifiedMarkdown = await getParsedMarkdown(params.slug.join("/"));
-  const siblingList = await getSiblingList(params.slug.slice(0, params.slug.length - 1).join("/"));
 
   return (
-    <div className="  ">
-      {siblingList.length > 0 && (
-        <div>
-          <div className="mb-4">
-            <span className=" text-xl border-b">관련있는글</span>
-          </div>
-          <div>
-            {siblingList.map((node) => (
-              <Link key={node.path} href={`/article/${node.path}`}>
-                <div key={node.path}>
-                  {node.name} {node.path}
-                </div>
-              </Link>
-            ))}
-          </div>
-        </div>
-      )}
-
+    <main className="  ">
       <div>
         <MarkdownContents markdown={modifiedMarkdown || ""} />{" "}
       </div>
-    </div>
+    </main>
   );
 };
 
