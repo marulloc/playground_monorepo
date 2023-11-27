8a372dbd06fb41321f2bcd0665290bda5687aa8d
marulloc <marullo@naver.com>
2023-11-27 17:14:54 +0900

    [style] article card

--- apps-marulloc/marulloc-blog/src/app/category/[...slug]/page.tsx
+++ apps-marulloc/marulloc-blog/src/app/category/[...slug]/page.tsx
@@ -64,14 +64,14 @@ const Page = async ({ params, searchParams }: TPageProps) => {
 
         <div>
           {curTabQuery === "article" && (
-            <section className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+            <section className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-12 mt-12   gap-y-16">
               {articleList.map((node) => (
                 <ArticleCard key={node.path} node={node} />
               ))}
             </section>
           )}
           {curTabQuery === "series" && (
-            <section className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-12 mt-12  ">
+            <section className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-12 mt-12   gap-y-16">
               {seriesList?.map((node, idx) => <SeriesCard node={node} key={`${node.path}-${idx}`} />)}
             </section>
           )}
@@ -86,27 +86,35 @@ export default Page;
 
 const ArticleCard = ({ node }: { node: AsyncFunctionValueType<ReturnType<typeof getArticleList>>[number] }) => {
   return (
-    <article className="   bg-white bg-opacity-20 backdrop-blur-sm border border-zinc-800 p-4 rounded-lg group hover:bg-zinc-900 hover:scale-105  transition-all duration-75   ">
-      <Link href={`/article/${node.path}`} className="flex flex-col items-start justify-between">
-        <div className="relative w-full  h-48  bg-zinc-800 rounded-lg ">
+    <Link
+      href={`/article/${node.path}`}
+      className=" relative group   hover:bg-zinc-400 hover:bg-opacity-10   border border-transparent hover:border-zinc-800 rounded-md p-6 -m-6 "
+    >
+      <div className="flex  items-start justify-between">
+        <div className={classNames(" h-40 w-full relative   bg-zinc-700 rounded-lg overflow-hidden   ")}>
           {node.contentImageUrl && (
-            <Image src={node.contentImageUrl} alt="" fill className="  w-full rounded-lg   object-cover " />
+            <Image
+              src={node.contentImageUrl}
+              fill
+              alt={""}
+              className=" object-cover object-center   shadow-lg   rounded-lg"
+            />
           )}
-          <div className="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
         </div>
-        <div className="max-w-xl">
-          <div className="  relative">
-            <h3 className="mt-3 text-lg font-semibold leading-6 text-zinc-300 group-hover:text-zinc-50">
-              <span className="absolute inset-0" />
-              {node.name}
-            </h3>
-            <div className="mt-5 line-clamp-3 text-sm leading-6 text-zinc-400">
-              <MarkdownContents markdown={node.contentDescription} />
-            </div>
-          </div>
+      </div>
+
+      {/*  */}
+      <div className=" mt-4 ">
+        <h2 className=" text-xl font-semibold   text-zinc-200 tracking-wide ">{node.name}</h2>
+
+        <div className="  line-clamp-3 text-sm leading-6 text-zinc-400 min-h-[20px] -my-2">
+          <MarkdownContents markdown={node.contentDescription} />
         </div>
-      </Link>
-    </article>
+        <div className="text-zinc-400 group-hover:text-pink-400 text-xs    ">
+          {/* {`view ${node.childDir.filter(({ name }) => name.toLowerCase() !== "readme.md").length} articles`} */}
+        </div>
+      </div>
+    </Link>
   );
 };
 
@@ -114,7 +122,7 @@ const SeriesCard = ({ node }: { node: AsyncFunctionValueType<ReturnType<typeof g
   return (
     <Link
       href={`/category/${node.path}`}
-      className=" relative group   hover:bg-zinc-900 hover:bg-opacity-30  border border-transparent hover:border-zinc-800 rounded-md p-6 -m-6 "
+      className=" relative group  hover:bg-zinc-400 hover:bg-opacity-10  border border-transparent hover:border-zinc-800 rounded-md p-6 -m-6 "
     >
       <div className="flex  items-start justify-between">
         <div className={classNames(" h-12 w-12 relative p-2 bg-zinc-700 rounded-full overflow-hidden   ")}>
