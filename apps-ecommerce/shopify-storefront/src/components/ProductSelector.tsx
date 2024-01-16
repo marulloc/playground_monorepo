c542750207bf8d483c7b087cb6341ded3ffc3e3b
marulloc <marullo@naver.com>
2024-01-16 19:43:32 +0900

    [feature] Product variant selector -function 2 : deselect and sync with dataUrl

--- apps-ecommerce/shopify-storefront/src/components/ProductSelector.tsx
+++ apps-ecommerce/shopify-storefront/src/components/ProductSelector.tsx
@@ -16,8 +16,10 @@ const ProductSelector = ({ product: { variants, options } }: Props) => {
     value: Product['options'][number]['values'][number],
   ) => {
     const searchParamsWithOption = new URLSearchParams(searchParams.toString());
+    const isAlreadyExist = searchParamsWithOption.get(name) === value;
 
-    searchParamsWithOption.set(name, value);
+    if (isAlreadyExist) searchParamsWithOption.delete(name);
+    else searchParamsWithOption.set(name, value);
 
     const search = searchParamsWithOption.toString();
     const query = search ? `?${search}` : '';
@@ -32,77 +34,31 @@ const ProductSelector = ({ product: { variants, options } }: Props) => {
           <dt className="mb-4 text-sm uppercase tracking-wide">{option.name}</dt>
 
           <dd className="flex flex-wrap gap-3">
-            {option.values.map((value) => (
-              <button
-                key={`product-option-${option.name}-${value}`}
-                onClick={() => handleSelect(option.name, value)}
-                className={classNames(
-                  'flex min-w-[48px] items-center justify-center rounded-lg border bg-neutral-100 px-3 py-2 text-sm dark:border-neutral-800 dark:bg-neutral-900',
+            {option.values.map((value) => {
+              const isActive = searchParams.get(option.name) === value;
 
-                  searchParams.get(option.name) === value
-                    ? 'cursor-default ring-2 ring-teal-600' // active option
-                    : 'ring-1 ring-transparent transition duration-300 ease-in-out hover:scale-110 hover:ring-teal-600 ',
-                )}
-              >
-                {value}
-              </button>
-            ))}
+              return (
+                <button
+                  key={`product-option-${option.name}-${value}`}
+                  onClick={() => handleSelect(option.name, value)}
+                  className={classNames(
+                    'flex min-w-[48px] items-center justify-center rounded-lg border bg-neutral-100 px-3 py-2 text-sm dark:border-neutral-800 dark:bg-neutral-900',
+
+                    isActive
+                      ? '  ring-2 ring-teal-600' // active option
+                      : '  ring-1 ring-transparent transition duration-300 ease-in-out hover:scale-110 hover:ring-teal-600 ',
+
+                    // not available style
+                    // 'relative z-10 cursor-not-allowed overflow-hidden bg-neutral-100 text-neutral-500 ring-1 ring-neutral-300 before:absolute before:inset-x-0 before:-z-10 before:h-px before:-rotate-45 before:bg-neutral-300 before:transition-transform dark:bg-neutral-900 dark:text-neutral-400 dark:ring-neutral-700 before:dark:bg-neutral-700',
+                  )}
+                >
+                  {value}
+                </button>
+              );
+            })}
           </dd>
         </dl>
       ))}
-
-      {/*  */}
-
-      <dl className="mb-8">
-        <dt className="mb-4 text-sm uppercase tracking-wide">{'Colors'}</dt>
-        <dd className="flex flex-wrap gap-3">
-          <button
-            // key={value}
-            // aria-disabled={!isAvailableForSale}
-            // disabled={!isAvailableForSale}
-            // onClick={() => {
-            //   // router.replace(optionUrl, { scroll: false });
-            // }}
-            title={`${'Colors'} ${'Red'}${!true ? ' (Out of Stock)' : ''}`}
-            className={classNames(
-              'flex min-w-[48px] items-center justify-center rounded-lg border bg-neutral-100 px-3 py-2 text-sm dark:border-neutral-800 dark:bg-neutral-900',
-
-              'ring-1 ring-transparent transition duration-300 ease-in-out hover:scale-110 hover:ring-teal-600 ',
-            )}
-          >
-            {'Red'}
-          </button>
-
-          <button
-            title={`${'Colors'} ${'Blue'}${!true ? ' (Out of Stock)' : ''}`}
-            className={classNames(
-              'flex min-w-[48px] items-center justify-center rounded-lg border bg-neutral-100 px-3 py-2 text-sm dark:border-neutral-800 dark:bg-neutral-900',
-
-              'cursor-default ring-2 ring-teal-600', // => active options인 경우
-              // 'ring-1 ring-transparent transition duration-300 ease-in-out hover:scale-110 hover:ring-blue-600 ',
-            )}
-          >
-            {'Blue'}
-          </button>
-
-          <button
-            title={`${'Colors'} ${'Blue'}${!true ? ' (Out of Stock)' : ''}`}
-            className={classNames(
-              'flex min-w-[48px] items-center justify-center rounded-lg border bg-neutral-100 px-3 py-2 text-sm dark:border-neutral-800 dark:bg-neutral-900',
-
-              'cursor-default ring-2 ring-blue-600',
-              // 'ring-1 ring-transparent transition duration-300 ease-in-out hover:scale-110 hover:ring-blue-600 ',
-              'relative z-10 cursor-not-allowed overflow-hidden bg-neutral-100 text-neutral-500 ring-1 ring-neutral-300 before:absolute before:inset-x-0 before:-z-10 before:h-px before:-rotate-45 before:bg-neutral-300 before:transition-transform dark:bg-neutral-900 dark:text-neutral-400 dark:ring-neutral-700 before:dark:bg-neutral-700',
-              // {
-              //   'relative z-10 cursor-not-allowed overflow-hidden bg-neutral-100 text-neutral-500 ring-1 ring-neutral-300 before:absolute before:inset-x-0 before:-z-10 before:h-px before:-rotate-45 before:bg-neutral-300 before:transition-transform dark:bg-neutral-900 dark:text-neutral-400 dark:ring-neutral-700 before:dark:bg-neutral-700':
-              //     !isAvailableForSale
-              // }
-            )}
-          >
-            {'White'}
-          </button>
-        </dd>
-      </dl>
     </>
   );
 };
