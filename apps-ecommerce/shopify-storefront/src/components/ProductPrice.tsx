6410795a4cc5da46eb069afafc1d315591b34853
marulloc <marullo@naver.com>
2024-01-22 01:02:40 +0900

    [feature] added title and price in Product Card

--- apps-ecommerce/shopify-storefront/src/components/ProductPrice.tsx
+++ apps-ecommerce/shopify-storefront/src/components/ProductPrice.tsx
@@ -1,16 +1,17 @@
 import { classNames } from '@/styles/utils';
 import Price from './Price';
 
-type Props = {
+type Props<T extends React.ElementType = 'div'> = {
   priceRange: Product['priceRange'];
-};
-const ProductPrice = ({ priceRange }: Props) => {
+} & Omit<React.ComponentPropsWithoutRef<T>, 'children' | 'ref'>;
+
+const ProductPrice = ({ priceRange, className = 'text-zinc-400', ...rest }: Props) => {
   const isSame = priceRange.maxVariantPrice.amount === priceRange.minVariantPrice.amount;
 
   return (
-    <div className="   ">
+    <div className={classNames(className)} {...rest}>
       {/* <span className="text-zinc-400 text-xs pb-1 "> Price Range </span> */}
-      <div className="flex space-x-2 text-zinc-400">
+      <div className="flex space-x-2  ">
         <div>
           <Price currencyCode={priceRange.minVariantPrice.currencyCode} amount={priceRange.minVariantPrice.amount} />
         </div>
